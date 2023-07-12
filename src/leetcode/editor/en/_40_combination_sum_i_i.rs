36f257d91175e89402838a925330fb09db430982
mike <lixiaofeng1115@outlook.com>
2023-07-12 17:45:52 +0800

    40: combination sum II, todo!

---
+++ src/leetcode/editor/en/_40_combination_sum_i_i.rs
@@ -0,0 +1,57 @@
+//Given a collection of candidate numbers (candidates) and a target number (
+//target), find all unique combinations in candidates where the candidate numbers sum
+//to target.
+//
+// Each number in candidates may only be used once in the combination.
+//
+// Note: The solution set must not contain duplicate combinations.
+//
+//
+// Example 1:
+//
+//
+//Input: candidates = [10,1,2,7,6,1,5], target = 8
+//Output:
+//[
+//[1,1,6],
+//[1,2,5],
+//[1,7],
+//[2,6]
+//]
+//
+//
+// Example 2:
+//
+//
+//Input: candidates = [2,5,2,1,2], target = 5
+//Output:
+//[
+//[1,2,2],
+//[5]
+//]
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= candidates.length <= 100
+// 1 <= candidates[i] <= 50
+// 1 <= target <= 30
+//
+//
+// Related Topics Array Backtracking 👍 8988 👎 228
+
+#![allow(dead_code)]
+
+pub struct Solution;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+impl Solution {
+    pub fn combination_sum2(_candidates: Vec<i32>, _target: i32) -> Vec<Vec<i32>> {
+        let res = vec![];
+        // todo!
+        res
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- src/leetcode/editor/en/mod.rs
+++ src/leetcode/editor/en/mod.rs
@@ -32,6 +32,7 @@ pub mod _37_sudoku_solver;
 pub mod _38_count_and_say;
 pub mod _39_combination_sum;
 pub mod _3_longest_substring_without_repeating_characters;
+pub mod _40_combination_sum_i_i;
 pub mod _4_median_of_two_sorted_arrays;
 pub mod _58_length_of_last_word;
 pub mod _5_longest_palindromic_substring;
