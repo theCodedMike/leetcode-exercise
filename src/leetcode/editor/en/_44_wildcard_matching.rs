f51c2b1d4efcfb164178b3020b965d5bb3bc3ef8
mike <lixiaofeng1115@outlook.com>
2023-07-15 17:08:16 +0800

    44: wildcard matching, todo!

--- src/leetcode/editor/en/_44_wildcard_matching.rs
+++ src/leetcode/editor/en/_44_wildcard_matching.rs
@@ -1,47 +1,47 @@
-//Given an input string (s) and a pattern (p), implement wildcard pattern 
-//matching with support for '?' and '*' where: 
+//Given an input string (s) and a pattern (p), implement wildcard pattern
+//matching with support for '?' and '*' where:
 //
-// 
-// '?' Matches any single character. 
-// '*' Matches any sequence of characters (including the empty sequence). 
-// 
 //
-// The matching should cover the entire input string (not partial). 
+// '?' Matches any single character.
+// '*' Matches any sequence of characters (including the empty sequence).
 //
-// 
-// Example 1: 
 //
-// 
+// The matching should cover the entire input string (not partial).
+//
+//
+// Example 1:
+//
+//
 //Input: s = "aa", p = "a"
 //Output: false
 //Explanation: "a" does not match the entire string "aa".
-// 
 //
-// Example 2: 
 //
-// 
+// Example 2:
+//
+//
 //Input: s = "aa", p = "*"
 //Output: true
 //Explanation: '*' matches any sequence.
-// 
 //
-// Example 3: 
 //
-// 
+// Example 3:
+//
+//
 //Input: s = "cb", p = "?a"
 //Output: false
-//Explanation: '?' matches 'c', but the second letter is 'a', which does not 
+//Explanation: '?' matches 'c', but the second letter is 'a', which does not
 //match 'b'.
-// 
 //
-// 
-// Constraints: 
 //
-// 
-// 0 <= s.length, p.length <= 2000 
-// s contains only lowercase English letters. 
-// p contains only lowercase English letters, '?' or '*'. 
-// 
+//
+// Constraints:
+//
+//
+// 0 <= s.length, p.length <= 2000
+// s contains only lowercase English letters.
+// p contains only lowercase English letters, '?' or '*'.
+//
 //
 // Related Topics String Dynamic Programming Greedy Recursion 👍 7288 👎 304
 
@@ -51,8 +51,9 @@ pub struct Solution;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 impl Solution {
-    pub fn is_match(s: String, p: String) -> bool {
-        
+    pub fn is_match(_s: String, _p: String) -> bool {
+        // todo!
+        false
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
