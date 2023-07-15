11c077348acfce824e7534c929f851a69d326cc9
mike <lixiaofeng1115@outlook.com>
2023-07-15 17:07:40 +0800

    44: wildcard matching, todo!

---
+++ src/leetcode/editor/en/_44_wildcard_matching.rs
@@ -0,0 +1,58 @@
+//Given an input string (s) and a pattern (p), implement wildcard pattern 
+//matching with support for '?' and '*' where: 
+//
+// 
+// '?' Matches any single character. 
+// '*' Matches any sequence of characters (including the empty sequence). 
+// 
+//
+// The matching should cover the entire input string (not partial). 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "aa", p = "a"
+//Output: false
+//Explanation: "a" does not match the entire string "aa".
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "aa", p = "*"
+//Output: true
+//Explanation: '*' matches any sequence.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = "cb", p = "?a"
+//Output: false
+//Explanation: '?' matches 'c', but the second letter is 'a', which does not 
+//match 'b'.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= s.length, p.length <= 2000 
+// s contains only lowercase English letters. 
+// p contains only lowercase English letters, '?' or '*'. 
+// 
+//
+// Related Topics String Dynamic Programming Greedy Recursion 👍 7288 👎 304
+
+#![allow(dead_code)]
+
+pub struct Solution;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+impl Solution {
+    pub fn is_match(s: String, p: String) -> bool {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- src/leetcode/editor/en/mod.rs
+++ src/leetcode/editor/en/mod.rs
@@ -36,6 +36,7 @@ pub mod _40_combination_sum_i_i;
 pub mod _41_first_missing_positive;
 pub mod _42_trapping_rain_water;
 pub mod _43_multiply_strings;
+pub mod _44_wildcard_matching;
 pub mod _4_median_of_two_sorted_arrays;
 pub mod _58_length_of_last_word;
 pub mod _5_longest_palindromic_substring;
