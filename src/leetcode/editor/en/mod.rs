40b1ce77e9d917f3188104b08097fbd8b73d2271
mike <lixiaofeng1115@outlook.com>
2023-07-26 15:56:55 +0800

    60: permutation sequence

--- src/leetcode/editor/en/mod.rs
+++ src/leetcode/editor/en/mod.rs
@@ -52,6 +52,7 @@ pub mod _57_insert_interval;
 pub mod _58_length_of_last_word;
 pub mod _59_spiral_matrix_i_i;
 pub mod _5_longest_palindromic_substring;
+pub mod _60_permutation_sequence;
 pub mod _66_plus_one;
 pub mod _67_add_binary;
 pub mod _69_sqrt_x;
