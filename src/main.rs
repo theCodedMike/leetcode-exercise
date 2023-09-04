b531438ff1f7d4f4438acc114b2b8dd769085a8d
mike <lixiaofeng1115@outlook.com>
2023-09-04 12:50:18 +0800

    147: insertion sort list

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
 fn main() {
     println!("Hello, world!");
-    let now = SystemTime::now();
-    let diff = now.duration_since(UNIX_EPOCH).unwrap_or_default();
-    println!("{}", diff.as_nanos());
 }
