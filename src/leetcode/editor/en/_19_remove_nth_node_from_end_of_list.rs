7032e91bd09994bdffc034ff135edd288a38ddd5
mike <lixiaofeng1115@outlook.com>
2023-10-24 20:15:12 +0800

    19: remove nth node from end of list

--- src/leetcode/editor/en/_19_remove_nth_node_from_end_of_list.rs
+++ src/leetcode/editor/en/_19_remove_nth_node_from_end_of_list.rs
@@ -60,26 +60,106 @@ impl ListNode {
 
 impl Solution {
     pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
-        // count of nodes
-        let mut count = 0;
-        let mut counter = head.as_ref();
-        while let Some(node) = counter {
-            count += 1;
-            counter = node.next.as_ref();
-        }
-        // move the pointer to node which before the deleted node
-        let mut new_head = ListNode::new(0);
-        new_head.next = head;
-        let mut helper = &mut new_head;
-        for _ in 0..(count - n) {
-            helper = helper.next.as_mut().unwrap();
-        }
-        // remove
-        helper.next.take().map(|target| {
-            helper.next = target.next;
-        });
+        //Self::calc_length_of_listed_list(head, n)
+        //Self::use_stack(head, n)
+        Self::two_pointers(head, n)
+        //Self::recursion_helper(head, n).1
+    }
 
-        new_head.next
+    /// Time Complexity: O(n)
+    ///
+    /// Space Complexity: O(1)
+    fn calc_length_of_listed_list(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        // calculate the len of linked list
+        let mut counter = head.as_ref();
+        let mut len = 0;
+        while let Some(curr) = counter {
+            len += 1;
+            counter = curr.next.as_ref();
+        }
+
+        let mut dummy = ListNode::new(-1);
+        dummy.next = head;
+        let mut p = &mut dummy;
+        // move p to the previous node of to be deleted node
+        for _ in 0..(len - n) {
+            p = p.next.as_mut().unwrap();
+        }
+        if let Some(mut to_be_del) = p.next.take() {
+            p.next = to_be_del.next.take();
+        }
+
+        dummy.next
+    }
+
+    /// Time Complexity: O(n)
+    ///
+    /// Space Complexity: O(n)
+    fn use_stack(mut head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        let mut stack = vec![];
+        while let Some(mut curr) = head {
+            head = curr.next.take();
+            stack.push(curr);
+        }
+
+        let mut i = 0;
+        let mut head = None;
+        while let Some(mut curr) = stack.pop() {
+            i += 1;
+            if i != n {
+                curr.next = head;
+                head = Some(curr);
+            }
+        }
+
+        head
+    }
+
+    /// Time Complexity: O(n)
+    ///
+    /// Space Complexity: O(1)
+    fn two_pointers(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        let mut dummy = ListNode::new(-1);
+        dummy.next = head;
+        let mut fast = &dummy as *const ListNode;
+        let mut slow = &mut dummy as *mut ListNode;
+
+        unsafe {
+            for _ in 0..n {
+                fast = (*fast).next.as_deref().unwrap();
+            }
+            loop {
+                if (*fast).next.is_none() {
+                    break;
+                }
+                fast = (*fast).next.as_deref().unwrap();
+                slow = (*slow).next.as_deref_mut().unwrap();
+            }
+            if let Some(mut to_be_del) = (*slow).next.take() {
+                (*slow).next = to_be_del.next.take();
+            }
+        }
+
+        dummy.next
+    }
+
+    /// Time Complexity: O(n)
+    ///
+    /// Space Complexity: O(n)
+    fn recursion_helper(head: Option<Box<ListNode>>, n: i32) -> (i32, Option<Box<ListNode>>) {
+        match head {
+            None => (0, None),
+            Some(mut curr) => {
+                let (level, next) = Self::recursion_helper(curr.next.take(), n);
+                curr.next = next;
+
+                if level + 1 == n {
+                    (level + 1, curr.next)
+                } else {
+                    (level + 1, Some(curr))
+                }
+            }
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
