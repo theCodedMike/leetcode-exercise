d732367cd0459e8027f47eba512fd19ddd158933
mike <lixiaofeng1115@outlook.com>
2023-12-01 20:03:41 +0800

    150: evaluate reverse polish notation

--- src/leetcode/editor/en/_150_evaluate_reverse_polish_notation.rs
+++ src/leetcode/editor/en/_150_evaluate_reverse_polish_notation.rs
@@ -64,36 +64,69 @@
 pub struct Solution;
 
 //leetcode submit region begin(Prohibit modification and deletion)
+const CALC: fn(i32, i32, &str) -> i32 = |left, right, operator| match operator {
+    "+" => left + right,
+    "-" => left - right,
+    "*" => left * right,
+    "/" => left / right,
+    _ => panic!("Unsupported operator: {}", operator),
+};
+
+const IS_OPERATOR: fn(&str) -> bool = |token| match token {
+    "+" | "-" | "*" | "/" => true,
+    _ => false,
+};
+
 impl Solution {
     pub fn eval_rpn(tokens: Vec<String>) -> i32 {
-        let mut stack = vec![];
+        Self::simulate_stack(tokens)
+        //Self::use_stack(tokens)
+    }
 
-        for token in &tokens {
-            let token = token.as_str();
-            match token {
-                "+" | "-" | "*" | "/" => {
-                    let r_operand = stack.pop().unwrap_or_default();
-                    let l_operand = stack.pop().unwrap_or_default();
-                    stack.push(Self::calc(l_operand, r_operand, token));
+    /// Time Complexity: O(n^2)
+    ///
+    /// Space Complexity: O(1)
+    fn simulate_stack(mut tokens: Vec<String>) -> i32 {
+        let len = tokens.len();
+
+        for i in 0..len {
+            if IS_OPERATOR(&tokens[i]) {
+                let mut r = i - 1;
+                while tokens[r].is_empty() {
+                    r -= 1;
                 }
-                _ => {
-                    let val = token.parse::<i32>().unwrap_or_default();
-                    stack.push(val);
+                let right = tokens[r].parse::<i32>().unwrap();
+                let mut l = r - 1;
+                while tokens[l].is_empty() {
+                    l -= 1;
                 }
+                let left = tokens[l].parse::<i32>().unwrap();
+                tokens[i] = CALC(left, right, &tokens[i]).to_string();
+                tokens[r].clear();
+                tokens[l].clear();
             }
         }
 
-        stack.pop().unwrap_or_default()
+        tokens[len - 1].parse::<i32>().unwrap()
     }
 
-    fn calc(l_operand: i32, r_operand: i32, operator: &str) -> i32 {
-        match operator {
-            "+" => l_operand + r_operand,
-            "-" => l_operand - r_operand,
-            "*" => l_operand * r_operand,
-            "/" => l_operand / r_operand,
-            _ => panic!("unsupported operator"),
+    /// Time Complexity: O(n)
+    ///
+    /// Space Complexity: O(n)
+    fn use_stack(tokens: Vec<String>) -> i32 {
+        let mut stack = Vec::with_capacity(tokens.len() / 2);
+
+        for ref token in tokens {
+            if IS_OPERATOR(token) {
+                let right = stack.pop().unwrap();
+                let left = stack.pop().unwrap();
+                stack.push(CALC(left, right, token));
+            } else {
+                stack.push(token.parse::<i32>().unwrap())
+            }
         }
+
+        stack[0]
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ tests/medium_150_test.rs
@@ -0,0 +1,47 @@
+use leetcode_exercise::leetcode::editor::en::_150_evaluate_reverse_polish_notation::Solution;
+
+#[test]
+fn evaluate_reverse_polish_notation_1() {
+    let tokens = vec![
+        "2".to_string(),
+        "1".to_string(),
+        "+".to_string(),
+        "3".to_string(),
+        "*".to_string(),
+    ];
+    let res = Solution::eval_rpn(tokens);
+    assert_eq!(res, 9);
+}
+
+#[test]
+fn evaluate_reverse_polish_notation_2() {
+    let tokens = vec![
+        "4".to_string(),
+        "13".to_string(),
+        "5".to_string(),
+        "/".to_string(),
+        "+".to_string(),
+    ];
+    let res = Solution::eval_rpn(tokens);
+    assert_eq!(res, 6);
+}
+#[test]
+fn evaluate_reverse_polish_notation_3() {
+    let tokens = vec![
+        "10".to_string(),
+        "6".to_string(),
+        "9".to_string(),
+        "3".to_string(),
+        "+".to_string(),
+        "-11".to_string(),
+        "*".to_string(),
+        "/".to_string(),
+        "*".to_string(),
+        "17".to_string(),
+        "+".to_string(),
+        "5".to_string(),
+        "+".to_string(),
+    ];
+    let res = Solution::eval_rpn(tokens);
+    assert_eq!(res, 22);
+}
