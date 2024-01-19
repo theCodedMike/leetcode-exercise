66cd4ca633caabb9c8f76c9b95d2013010db5a08
mike <lixiaofeng1115@outlook.com>
2024-01-19 17:03:56 +0800

    108: convert sorted array to binary search tree

--- src/leetcode/editor/cn/_108_convert_sorted_array_to_binary_search_tree.rs
+++ src/leetcode/editor/cn/_108_convert_sorted_array_to_binary_search_tree.rs
@@ -34,124 +34,115 @@
 #![allow(dead_code)]
 
 pub struct Solution;
-
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
+use crate::binary_tree::safe::TreeNode;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-
 use std::cell::RefCell;
 use std::collections::VecDeque;
-use std::ops::Index;
 use std::rc::Rc;
 impl Solution {
     pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        //Self::recursion_helper(&nums)
-        //Self::iteration_helper_queue(nums)
-        Self::iteration_helper_stack(nums)
+        //Self::recur_helper_1(nums)
+        //Self::recur_helper_2(nums)
+
+        //Self::iter_helper_1(nums)
+        Self::iter_helper_2(nums)
     }
 
-    fn recursion_helper(nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
-        let len = nums.len();
-        if len == 0 {
+    fn recur_helper_1(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        const CONVERT: fn(&[i32]) -> Option<Rc<RefCell<TreeNode>>> = |nums| {
+            let len = nums.len();
+            if len == 0 {
+                return None;
+            }
+
+            let mid = len / 2;
+            let root = Rc::new(RefCell::new(TreeNode::new(nums[mid])));
+            if len == 1 {
+                return Some(root);
+            }
+
+            root.borrow_mut().left = CONVERT(&nums[..mid]);
+            root.borrow_mut().right = CONVERT(&nums[mid + 1..]);
+
+            Some(root)
+        };
+
+        CONVERT(&nums)
+    }
+
+    fn recur_helper_2(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        const CONVERT: fn(&[i32], usize, usize) -> Option<Rc<RefCell<TreeNode>>> =
+            |nums, left, right| {
+                if left == right {
+                    return None;
+                }
+
+                let mid = (left + right) / 2;
+                let root = Rc::new(RefCell::new(TreeNode::new(nums[mid])));
+                if right - left == 1 {
+                    return Some(root);
+                }
+
+                root.borrow_mut().left = CONVERT(&nums, left, mid);
+                root.borrow_mut().right = CONVERT(&nums, mid + 1, right);
+
+                Some(root)
+            };
+
+        CONVERT(&nums, 0, nums.len())
+    }
+
+    fn iter_helper_1(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        if nums.is_empty() {
             return None;
         }
 
-        let root_idx = len / 2;
-        let root = Rc::new(RefCell::new(TreeNode::new(nums[root_idx])));
-        if len == 1 {
-            return Some(root);
+        let root = Rc::new(RefCell::new(TreeNode::new(nums[0])));
+        let mut queue = VecDeque::from([(root.clone(), 0, nums.len())]);
+
+        while let Some((curr, l_idx, r_idx)) = queue.pop_front() {
+            let mid = (l_idx + r_idx) / 2;
+            curr.borrow_mut().val = nums[mid];
+
+            if l_idx < mid {
+                let l_node = Rc::new(RefCell::new(TreeNode::new(nums[0])));
+                curr.borrow_mut().left = Some(l_node.clone());
+                queue.push_back((l_node, l_idx, mid));
+            }
+
+            if mid + 1 < r_idx {
+                let r_node = Rc::new(RefCell::new(TreeNode::new(nums[0])));
+                curr.borrow_mut().right = Some(r_node.clone());
+                queue.push_back((r_node, mid + 1, r_idx));
+            }
         }
-        root.borrow_mut().left = Self::recursion_helper(nums.index(..root_idx));
-        root.borrow_mut().right = Self::recursion_helper(nums.index(root_idx + 1..));
 
         Some(root)
     }
 
-    fn iteration_helper_queue(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        let len = nums.len();
-        if len == 0 {
+    fn iter_helper_2(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        if nums.is_empty() {
             return None;
         }
-        let root = Rc::new(RefCell::new(TreeNode::new(nums[0])));
-        if len == 1 {
-            return Some(root);
-        }
 
-        let mut node_q = VecDeque::from([root.clone()]);
-        let mut start_q = VecDeque::from([0]);
-        let mut end_q = VecDeque::from([len]);
-        while let Some(curr) = node_q.pop_front() {
-            let start = start_q.pop_front().unwrap();
-            let end = end_q.pop_front().unwrap();
-            let mid = start + (end - start) / 2;
+        let root = Rc::new(RefCell::new(TreeNode::new(nums[0])));
+        let mut stack = vec![(root.clone(), 0, nums.len())];
+
+        while let Some((curr, l_idx, r_idx)) = stack.pop() {
+            let mid = (l_idx + r_idx) / 2;
             curr.borrow_mut().val = nums[mid];
 
-            let l_start = start;
-            let l_end = mid;
-            if l_start < l_end {
-                let left = Rc::new(RefCell::new(TreeNode::new(nums[0])));
-                curr.borrow_mut().left = Some(left.clone());
-                node_q.push_back(left);
-                start_q.push_back(l_start);
-                end_q.push_back(l_end);
+            if l_idx < mid {
+                let l_node = Rc::new(RefCell::new(TreeNode::new(nums[0])));
+                curr.borrow_mut().left = Some(l_node.clone());
+                stack.push((l_node, l_idx, mid));
             }
 
-            let r_start = mid + 1;
-            let r_end = end;
-            if r_start < r_end {
-                let right = Rc::new(RefCell::new(TreeNode::new(nums[0])));
-                curr.borrow_mut().right = Some(right.clone());
-                node_q.push_back(right);
-                start_q.push_back(r_start);
-                end_q.push_back(r_end);
-            }
-        }
-
-        Some(root)
-    }
-
-    fn iteration_helper_stack(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        let len = nums.len();
-        if len == 0 {
-            return None;
-        }
-        let root = Rc::new(RefCell::new(TreeNode::new(nums[0])));
-        if len == 1 {
-            return Some(root);
-        }
-
-        let mut stack = vec![(root.clone(), 0, len)];
-        while let Some((curr, start, end)) = stack.pop() {
-            let mid = start + (end - start) / 2;
-            curr.borrow_mut().val = nums[mid];
-
-            if start < mid {
-                let left = Rc::new(RefCell::new(TreeNode::new(nums[0])));
-                curr.borrow_mut().left = Some(left.clone());
-                stack.push((left, start, mid))
-            }
-
-            if mid + 1 < end {
-                let right = Rc::new(RefCell::new(TreeNode::new(nums[0])));
-                curr.borrow_mut().right = Some(right.clone());
-                stack.push((right, mid + 1, end));
+            if mid + 1 < r_idx {
+                let r_node = Rc::new(RefCell::new(TreeNode::new(nums[0])));
+                curr.borrow_mut().right = Some(r_node.clone());
+                stack.push((r_node, mid + 1, r_idx));
             }
         }
 
--- tests/easy/easy500/easy_108_test.rs
+++ tests/easy/easy500/easy_108_test.rs
@@ -1,8 +1,29 @@
+use leetcode_exercise::binary_tree::safe::BinaryTree;
 use leetcode_exercise::leetcode::editor::cn::_108_convert_sorted_array_to_binary_search_tree::Solution;
+use leetcode_exercise::BuildTree;
 
 #[test]
-fn convert_sorted_array_to_binary_search_tree() {
+fn convert_sorted_array_to_binary_search_tree_1() {
     let nums = vec![-10, -3, 0, 5, 9];
-    let root = Solution::sorted_array_to_bst(nums);
-    println!("{:#?}", root);
+    let res = Solution::sorted_array_to_bst(nums);
+    //       0
+    //      / \
+    //    -3   9
+    //   /    /
+    // -10   5
+    let nodes = [Some(0), Some(-3), Some(9), Some(-10), None, Some(5)];
+    let exp = BinaryTree::build(&nodes);
+    assert_eq!(res, exp);
+}
+
+#[test]
+fn convert_sorted_array_to_binary_search_tree_2() {
+    let nums = vec![1, 3];
+    let res = Solution::sorted_array_to_bst(nums);
+    //       3
+    //      /
+    //     1
+    let nodes = [Some(3), Some(1)];
+    let exp = BinaryTree::build(&nodes);
+    assert_eq!(res, exp);
 }
